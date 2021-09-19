af6d4a6cd2d98440df6bf2d23fe4fd2835078150
Azat Imaev <41021025+RIDOS@users.noreply.github.com>
2021-09-19 12:29:34 +0500

    Update Program.cs

--- Second lab/Program.cs
+++ Second lab/Program.cs
@@ -89,9 +89,9 @@ namespace lab_2
         }
 		
 		public double Diametr()
-		{
-			return 2 * Math.PI* Radius();
-		}
+	{
+		return 2 * Math.PI* Radius();
+	}
         
         public new void Print()
         {
