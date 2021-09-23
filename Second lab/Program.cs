fcdc4f4de2f9a890bd599b9ee74621bb9612d476
RIDOS <guzairov99@bk.ru>
2021-09-23 18:34:41 +0500

    #2 Remaster lab 2

--- Second lab/Program.cs
+++ Second lab/Program.cs
@@ -101,14 +101,113 @@ namespace lab_2
                 Console.WriteLine("Точки не образуют окружность.");
         }
     }
-
+	
+	public class Fly
+	{
+		public string name;
+		public Fly(string Name)
+		{
+			this.name = Name;
+		}
+	}
+	
+	public class BigFly : Fly 
+	{
+		public string type_dvig;
+		public string type_corp;
+		public int dlina;
+		
+		public BigFly(string Name,string Dvigatel, string Salon, int Dlina) : base(Name)
+		{
+			this.type_dvig = Dvigatel;
+			this.type_corp = Salon;
+			this.dlina = Dlina;
+		}
+		
+		public void print()
+		{
+			Console.WriteLine("Название самолета: {0}\nТип двигателя: {1}\nТип салона: {2}\nДальность полета: {3} км/ч.", name, type_dvig, type_dvig, dlina);
+		}
+	}
+	
+	public class Deltaplan : Fly
+	{
+		public string model;
+		public string construktor;
+		
+		public Deltaplan(string Name, string Model, string Constructor): base(Name)
+		{
+			
+			this.model = Model;
+			this.construktor = Constructor;
+		}
+		
+		public void print()
+		{
+			Console.WriteLine("Название сдельтаплана: {0}\nМодель: {1}\nКонструктор: {2}", name, model, construktor);
+		}
+	}
+	
+	public class CosmoStar : Fly
+	{
+		public string type_cor;
+		public double weight;
+		
+		public CosmoStar(string Name, string Corpus, double Weight) : base(Name)
+		{
+			this.type_cor = Corpus;
+			this.weight = Weight;
+		}
+		
+		public void print()
+		{
+			Console.WriteLine("Навзвание космического аппарата: {0}\nТип корпуса: {1}\nВес: {2}", name, type_cor, weight);
+		}
+	}
+	
     class Program
     {
         static void Main(string[] args)
         {
-            Circle sm = new Circle(-6, 3, -3, 2, 0, 3);
-            sm.Print();
-            Console.WriteLine();
+			/*
+			 *  Point p = new Point(2, 3);
+			 *  p.Print();
+			 *  
+			 *  Segment sm = new Segment(2,3, 8, 10)
+			 *  sm.Print();
+			 * 
+			 *	Circle sm = new Circle(-6, 3, -3, 2, 0, 3);
+			 *	sm.Print();
+			 */
+			BigFly bf = new BigFly("Туполев Ту-154", "Реактивный", "пассажирский ", 3800);
+			Deltaplan delta = new Deltaplan("Target", "Cross-Country", "AEROS");
+			CosmoStar cs = new CosmoStar("Восход", "орбитальный", 177.5);
+			Deltaplan delta_1 = new Deltaplan("Stealth Combat GT 12,4", "Cross-Country", "AEROS");
+			BigFly bf_1 = new BigFly("Туполев Ту-134", "Реактивный", "пассажирский ", 5100);
+			
+			Fly[] fl = new Fly[]
+			{
+				bf,
+				delta,
+				cs,
+				delta_1,
+				bf_1
+			};
+			
+			for(int i = 0; i < fl.Length; i++)
+			{
+				Console.WriteLine("#{0} название летательного аппарата: {1}", i+1, fl[i].name);
+			}
+			
+			Console.WriteLine();
+			
+			bf.print();
+			delta.print();
+			cs.print();
+			delta_1.print();
+			bf_1.print();
+			
+            Console.ReadKey();
         }
     }
 }
